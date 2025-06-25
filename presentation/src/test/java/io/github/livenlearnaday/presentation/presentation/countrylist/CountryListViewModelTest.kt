3ab97a4be8b8d3b20a9a472026b0e868d92c85ea
livenlearnaday <39303203+livenlearnaday@users.noreply.github.com>
2025-06-25 12:56:04 +0700

    chore: update countryList tests (#12)

--- presentation/src/test/java/io/github/livenlearnaday/presentation/presentation/countrylist/CountryListViewModelTest.kt
+++ presentation/src/test/java/io/github/livenlearnaday/presentation/presentation/countrylist/CountryListViewModelTest.kt
@@ -1,9 +1,15 @@
 package io.github.livenlearnaday.presentation.countrylist
 
 import com.google.common.truth.Truth.assertThat
+import io.github.livenlearnaday.domain.CheckResult
 import io.github.livenlearnaday.domain.countrylist.model.CountryModel
+import io.github.livenlearnaday.domain.countrylist.usecase.ClearAllCountriesFavUseCase
+import io.github.livenlearnaday.domain.countrylist.usecase.DeleteAllCountriesUseCase
 import io.github.livenlearnaday.domain.countrylist.usecase.FetchCountriesFromApiUseCase
 import io.github.livenlearnaday.domain.countrylist.usecase.FetchCountriesFromDbUseCase
+import io.github.livenlearnaday.domain.countrylist.usecase.FetchCountriesSearchedUseCase
+import io.github.livenlearnaday.domain.countrylist.usecase.SaveCountriesUseCase
+import io.github.livenlearnaday.domain.countrylist.usecase.UpdateCountryFavUseCase
 import io.github.livenlearnaday.presentation.mockdata.CountryMockData
 import io.mockk.coEvery
 import io.mockk.mockk
@@ -21,6 +27,11 @@ class CountryListViewModelTest {
     private lateinit var countryList: List<CountryModel>
     private val fetchCountriesFromApiUseCase: FetchCountriesFromApiUseCase = mockk()
     private val fetchCountriesFromDbUseCase: FetchCountriesFromDbUseCase = mockk()
+    private val saveCountriesUseCase: SaveCountriesUseCase = mockk()
+    private val updateCountryFavUseCase: UpdateCountryFavUseCase = mockk()
+    private val fetchCountriesSearchedUseCase: FetchCountriesSearchedUseCase = mockk()
+    private val clearAllCountriesFavUseCase: ClearAllCountriesFavUseCase = mockk()
+    private val deleteAllCountriesUseCase: DeleteAllCountriesUseCase = mockk()
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Before
@@ -30,11 +41,11 @@ class CountryListViewModelTest {
         viewModel = CountryListViewModel(
             fetchCountriesFromApiUseCase,
             fetchCountriesFromDbUseCase,
-            mockk(),
-            mockk(),
-            mockk(),
-            mockk(),
-            mockk()
+            saveCountriesUseCase,
+            updateCountryFavUseCase,
+            fetchCountriesSearchedUseCase,
+            clearAllCountriesFavUseCase,
+            deleteAllCountriesUseCase
         )
     }
 
@@ -45,6 +56,42 @@ class CountryListViewModelTest {
 
         // Act
         viewModel.countryListAction(CountryListAction.FetchData)
+
+        // Assert
+        val countryStateCountry = viewModel.countryListState.countryItems.first()
+        val country = countryList.first()
+
+        assertThat(countryStateCountry.name).isEqualTo(country.name)
+        assertThat(countryStateCountry.capital).isEqualTo(country.capital)
+        assertThat(countryStateCountry.flag).isEqualTo(country.flag)
+    }
+
+    @Test
+    fun `should fetch from the network`() = runTest {
+        // Arrange
+        coEvery { fetchCountriesFromDbUseCase.execute() } returns flowOf(emptyList())
+        coEvery { fetchCountriesFromApiUseCase.execute() } returns CheckResult.Success(countryList)
+
+        // Act
+        viewModel.countryListAction(CountryListAction.FetchData)
+
+        // Assert
+        val countryStateCountry = viewModel.countryListState.countryItems.first()
+        val country = countryList.first()
+
+        assertThat(countryStateCountry.name).isEqualTo(country.name)
+        assertThat(countryStateCountry.capital).isEqualTo(country.capital)
+        assertThat(countryStateCountry.flag).isEqualTo(country.flag)
+    }
+
+    @Test
+    fun `refetch data from network`() = runTest {
+        // Arrange
+        coEvery { deleteAllCountriesUseCase.execute() } returns Unit
+        coEvery { fetchCountriesFromApiUseCase.execute() } returns CheckResult.Success(countryList)
+
+        // Act
+        viewModel.countryListAction(CountryListAction.OnFetchCountryListFromApi)
 
         // Assert
         val countryStateCountry = viewModel.countryListState.countryItems.first()
